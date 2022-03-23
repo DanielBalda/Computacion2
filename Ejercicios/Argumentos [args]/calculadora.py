f08da12db7ad36c5efd63cb6c84a675ca5e1afb8
DanielBalda <d.balda@alumno.um.edu.ar>
2022-03-23 14:56:10 -0300

    arreglos varios

--- Ejercicios/Argumentos [args]/calculadora.py
+++ Ejercicios/Argumentos [args]/calculadora.py
@@ -2,43 +2,44 @@ import sys
 import getopt
 
 
-def tryNumber(ar):
-    try:
-        return int(ar)
-    except ValueError:
-        print("Numero Invalido. Solo enteros")
-        exit()
+class InvalidNumber(Exception):
+    pass
 
 
 def getArg(opt):
     try:
-        operator = ''
-        numA = ''
-        numB = ''
         for op, ar in opt:
             if op == '-o':
                 if(ar not in "*/+-"):
                     raise ValueError()
                 operator = ar
             elif op == '-n':
-                numA = tryNumber(ar)
+                if ar.isnumeric():
+                    numA = ar
+                else:
+                    raise InvalidNumber()
             else:
-                numB = tryNumber(ar)
+                if ar.isnumeric():
+                    numB = ar
+                else:
+                    raise InvalidNumber()
         return print(eval(str(numA)+operator+str(numB)))
     except ValueError:
         print("Operador Invalido. Solo (*, /, -, +)")
+    except InvalidNumber:
+        print("Deben ser enteros.")
 
 
 def main():
     try:
         opt, _ = getopt.getopt(sys.argv[1:], 'o:n:m:')
         if opt == [] or len(opt) < 3:
-            return print("Ingrese los argumentos necesarios")
+            return print("Opciones/Argumentos incompletos.")
         getArg(opt)
     except getopt.GetoptError:
-        print("Ingrese los argumentos necesarios")
+        print("Ingrese opciones correctas.")
         exit()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
--- Ejercicios/Argumentos [args]/copiar.py
+++ Ejercicios/Argumentos [args]/copiar.py
@@ -2,23 +2,33 @@ import argparse
 import os
 
 
-class InvalidFileName(Exception):
+class FileNotExist(Exception):
     pass
+
 
 parser = argparse.ArgumentParser(description="Copia de un archivos Origen a uno Destino")
 
-parser.add_argument("-i", "--origen", type=str, required=True, help="Archivo Origen")
-parser.add_argument("-o", "--destino", type=str, required=True, help="Archivo Destino")
+parser.add_argument("-i",
+                    "--origen",
+                    type=str,
+                    required=True,
+                    help="Archivo Origen")
+parser.add_argument("-o",
+                    "--destino",
+                    type=str,
+                    required=True,
+                    help="Archivo Destino")
 args = parser.parse_args()
 
 try:
-    if(not os.path.exists(args.fileA)):
-        raise InvalidFileName()
-    with open(args.fileA, 'r') as f:
-        lines = f.readlines()
-except InvalidFileName:
+    if(not os.path.exists(args.origen)):
+        raise FileNotExist()
+except FileNotExist:
     print("El archivo origen no existe")
     exit()
-
-with open(args.fileB, 'w+') as f:
-    f.writelines(lines)
+origen = os.open(args.origen, os.O_RDONLY)
+destino = os.open(args.destino, os.O_WRONLY|os.O_CREAT)
+os.write(destino, os.read(origen, os.path.getsize(origen)))
+os.close(origen)
+os.close(destino)
+print("Copia finalizada.")
