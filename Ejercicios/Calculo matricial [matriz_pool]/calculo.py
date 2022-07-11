85e8e951d23020fd58511df3ebaec81f4055d2c3
DanielBalda <d.balda@alumno.um.edu.ar>
2022-07-11 15:23:18 -0300

    calculadora matricial terminada

--- Ejercicios/Calculo matricial [matriz_pool]/calculo.py
+++ Ejercicios/Calculo matricial [matriz_pool]/calculo.py
@@ -1,6 +1,8 @@
 import argparse
-import os
+import time
+import math
 from multiprocessing import Pool
+from functools import partial
 
 def main():
     parser = argparse.ArgumentParser(description="Comandos")
@@ -18,25 +20,37 @@ def main():
                         "--calculate",
                         type=str,
                         required=False,
-                        help="Operation")
+                        help="Operation: 'raiz', 'pot', 'log'")
     args = parser.parse_args()
     pool = Pool(processes=args.process)
-    lista = openFile(args.file)
-    result = pool.map(funcion_calculo, lista)
-    print(result)
+    matrix = openFile(args.file)
+    result = pool.map(partial(calculate, args.calculate), matrix)
+    for row in result:
+        print(', '.join(str(number) for number in row))
 
 def openFile(file):
     matrix=[]
     with open(file, 'r') as f:
         for line in f.readlines():
-            matrix.append(list(line.strip().replace(",","").replace(" ", "")))
+            line = line.strip().split(",")
+            matrix.append(line)
     return matrix
 
-def funcion_calculo(row):
+def calculate(operation, row):
+    time.sleep(.5)
     result=[]
-    for num in row:
-        num=int(num)
-        result.append(num*num)
+    if(operation == 'raiz'):
+        for num in row:
+            num=int(num)
+            result.append(round(math.sqrt(num), 2))
+    if(operation == 'pot'):
+        for num in row:
+            num=int(num)
+            result.append(num*num)
+    if(operation == 'log'):
+        for num in row:
+            num=int(num)
+            result.append(round(math.log10(num), 2))
     return result
 
 
