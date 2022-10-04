31b3f275eda5e4143e12465177750d8550514eb9
DanielBalda <d.balda@alumno.um.edu.ar>
2022-10-03 22:15:25 -0300

    ejercico Remote Shell [ServerSocket] terminado

---
+++ Ejercicios/Remote shell [serversocket]/cliente.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+import argparse
+import sys
+    
+def client(h,p):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error:
+        print("Socket creation failed")
+        sys.exit()
+    
+    host, port = h, p
+    print("Connecting..")
+    s.connect((host,port))
+    while True:
+        msg = input("Ingrese comando a ejecutar:")
+        if msg == "exit server":
+            print("Conexion finalizada...")
+            s.close()
+            exit()
+        s.send(msg.encode())
+        print("Esperando respuesta del server")
+        msg = s.recv(1024)
+        print("\n"+msg.decode())
+    
+def main():
+    parser = argparse.ArgumentParser(description="Comandos")
+    parser.add_argument("-ht","--host",required=True,help="Dirección IP o nombre del servidor al que conectarse")
+    parser.add_argument("-p","--port", type=int,required=True,help="Numero de puerto del servidor")
+    args = parser.parse_args()
+    print(args.host,args.port)
+    client(args.host,args.port)
+
+if __name__ == '__main__':
+    main()
---
+++ Ejercicios/Remote shell [serversocket]/server.py
@@ -0,0 +1,53 @@
+import socketserver,subprocess
+import signal,os,argparse
+
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        while True:
+            self.data = self.request.recv(1024).strip()
+            if( not self.data):
+                server.shutdown()
+                server.close_request()
+                exit()
+            pp = subprocess.Popen([self.data], stdout = subprocess.PIPE,stderr = subprocess.PIPE, shell = True)
+            out,err = pp.communicate()
+            if err:
+                self.request.sendall(b"## ERROR ##\n"+err)
+                print(err)
+            else:
+                self.request.sendall(b"## OK ##\n"+out)
+                print(out)
+            
+            print("PID: %d" % os.getpid())
+
+class ProcTCPServer(socketserver.ForkingMixIn, socketserver.TCPServer):
+    pass
+
+class ThrTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Comandos")
+    parser.add_argument("-p","--port", type=int,required=True,help="Puerto a trabajar")
+    parser.add_argument("-c","--concurrence", type=str ,required=True,help="P o T -> P = proceso, T = Hilo")
+    args = parser.parse_args()
+    HOST, PORT = "localhost", args.port
+    print("Server iniciado en: ",HOST," - Puerto: ",PORT)
+    socketserver.TCPServer.allow_reuse_address = True
+
+    if args.concurrence.lower() == 't':
+        with ThrTCPServer((HOST, PORT), MyTCPHandler) as server:
+            server.serve_forever()
+            try:
+                signal.pause()
+            except:
+                server.shutdown()
+
+    if args.concurrence.lower() == 'p':
+        with ProcTCPServer((HOST, PORT), MyTCPHandler) as server:
+            server.serve_forever()
+            try:
+                signal.pause()
+            except:
+                server.shutdown()
